7b2e4b24e1665e223b742c0a8daa31d5ffe47cef
charexcalibur <charexcalibur@gmail.com>
2017-12-27 21:52:32 +0800

    提交题目添加二次确认

--- src/components/upload.vue
+++ src/components/upload.vue
@@ -1,43 +1,53 @@
 <template>
   <el-form :model="questionsForm" :rules="rules" ref="questionsForm" label-width="100px" class="demo-questionsForm">
-  <el-form-item label="题目编号" prop="questionId">
-    <el-input v-model="questionsForm.questionId"></el-input>
-  </el-form-item>
-  <el-form-item label="题目等级" prop="questionLevel">
-    <el-input v-model="questionsForm.questionLevel" placeholder="等级"></el-input>
-  </el-form-item>
-  <el-form-item label="题目单元" prop="questionUnit">
-    <el-input v-model="questionsForm.questionUnit" placeholder="单元"></el-input>
-  </el-form-item>
-  <el-form-item label="题目标题" prop="questionTitle">
-    <el-input v-model="questionsForm.questionTitle"></el-input>
-  </el-form-item>
-  <el-form-item label="选项" prop="option_1">
-    <el-input v-model="questionsForm.option_1" placeholder="选项1"></el-input>
-  </el-form-item>
-  <el-form-item label="选项" prop="option_2">
-    <el-input v-model="questionsForm.option_2" placeholder="选项2"></el-input>
-  </el-form-item>
-  <el-form-item label="选项" prop="option_3">
-    <el-input v-model="questionsForm.option_3" placeholder="选项3"></el-input>
-  </el-form-item>
-  <el-form-item label="选项" prop="option_4">
-    <el-input v-model="questionsForm.option_4" placeholder="选项4"></el-input>
-  </el-form-item>
-  <el-form-item label="答案" prop="correctAnswer">
-    <el-radio v-model="questionsForm.correctAnswer" label="1">答案1</el-radio>
-    <el-radio v-model="questionsForm.correctAnswer" label="2">答案2</el-radio>
-    <el-radio v-model="questionsForm.correctAnswer" label="3">答案3</el-radio>
-    <el-radio v-model="questionsForm.correctAnswer" label="4">答案4</el-radio>
-  </el-form-item>
-  <el-form-item label="答案解析" prop="answerAnalysis">
-    <el-input type="textarea" v-model="questionsForm.answerAnalysis"></el-input>
-  </el-form-item>
-  <el-form-item>
-    <el-button type="primary" @click="submitForm('questionsForm')">立即创建</el-button>
-    <el-button @click="resetForm('questionsForm')">重置</el-button>
-  </el-form-item>
-</el-form>
+    <el-form-item label="题目编号" prop="questionId">
+      <el-input v-model="questionsForm.questionId"></el-input>
+    </el-form-item>
+    <el-form-item label="题目等级" prop="questionLevel">
+      <el-input v-model="questionsForm.questionLevel" placeholder="等级"></el-input>
+    </el-form-item>
+    <el-form-item label="题目单元" prop="questionUnit">
+      <el-input v-model="questionsForm.questionUnit" placeholder="单元"></el-input>
+    </el-form-item>
+    <el-form-item label="题目标题" prop="questionTitle">
+      <el-input v-model="questionsForm.questionTitle"></el-input>
+    </el-form-item>
+    <el-form-item label="选项" prop="option_1">
+      <el-input v-model="questionsForm.option_1" placeholder="选项1"></el-input>
+    </el-form-item>
+    <el-form-item label="选项" prop="option_2">
+      <el-input v-model="questionsForm.option_2" placeholder="选项2"></el-input>
+    </el-form-item>
+    <el-form-item label="选项" prop="option_3">
+      <el-input v-model="questionsForm.option_3" placeholder="选项3"></el-input>
+    </el-form-item>
+    <el-form-item label="选项" prop="option_4">
+      <el-input v-model="questionsForm.option_4" placeholder="选项4"></el-input>
+    </el-form-item>
+    <el-form-item label="答案" prop="correctAnswer">
+      <el-radio v-model="questionsForm.correctAnswer" label="1">答案1</el-radio>
+      <el-radio v-model="questionsForm.correctAnswer" label="2">答案2</el-radio>
+      <el-radio v-model="questionsForm.correctAnswer" label="3">答案3</el-radio>
+      <el-radio v-model="questionsForm.correctAnswer" label="4">答案4</el-radio>
+    </el-form-item>
+    <el-form-item label="答案解析" prop="answerAnalysis">
+      <el-input type="textarea" v-model="questionsForm.answerAnalysis"></el-input>
+    </el-form-item>
+    <el-dialog
+      title="提示"
+      :visible.sync="dialogVisible"
+      width="30%">
+      <span>确定提交吗？</span>
+      <span slot="footer" class="dialog-footer">
+        <el-button type="primary" @click="submitForm('questionsForm')">立即创建</el-button>
+        <el-button type="primary" @click="dialogVisible = false">取消</el-button>
+      </span>
+    </el-dialog>
+    <el-form-item>
+      <el-button type="primary" @click="dialogVisible = true">确定</el-button>
+      <el-button @click="resetForm('questionsForm')">重置</el-button>
+    </el-form-item>
+  </el-form>
 </template>
 
 <script>
@@ -46,6 +56,8 @@
   export default {
     data () {
       return {
+        dialogVisible: false,
+
         questionsForm: {
           questionId: '',
           questionLevel: '',
@@ -94,7 +106,7 @@
     },
     methods: {
       submitForm (formName) {
-        this.$refs[formName].validate(async (valid) => {
+        this.$refs[formName].validate((valid) => {
           if (valid) {
             const params = {
               questionId: this.questionsForm.questionId,
@@ -117,6 +129,7 @@
             })
 
             this.resetForm(formName)
+            this.dialogVisible = false
           } else {
             console.log('error submit!!')
             return false
