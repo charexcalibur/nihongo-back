d5e9ba0d8cfdf5a54ea8b934119a2efec04401d2
charexcalibur <charexcalibur@gmail.com>
2018-01-14 16:51:22 +0800

    add login keyup enter

--- src/page/login.vue
+++ src/page/login.vue
@@ -6,7 +6,7 @@
           <el-input v-model="formData.user_name"></el-input>
         </el-form-item>
         <el-form-item label="密码" prop="password">
-          <el-input v-model="formData.password" type="password"></el-input>
+          <el-input v-model="formData.password" type="password" @keyup.enter.native="login"></el-input>
         </el-form-item>
       </el-form>
       <div class="btn">
